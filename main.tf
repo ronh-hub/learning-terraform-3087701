bf2b3929f4047788b1c8f4150a5f448956e250b8
ronh-hub <sirronaldoh@gmail.com>
2025-01-27 17:55:27 +0000

    fiix error with arrays

--- main.tf
+++ main.tf
@@ -51,9 +51,11 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog_alb"
+  load_balancer_type = "application"
+
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_group = module.blog_sg.security_group_id
+  security_group = [module.blog_sg.security_group_id]
 
 
   target_groups = [
